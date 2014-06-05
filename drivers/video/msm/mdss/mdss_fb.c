377bf3e327978e94e4e09f8205598a14d592ee1a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-06-05 04:43:31 +0100

    Revert "msm: Fix high load average from uninterruptible waits (@cyanogen)"
    This reverts commit d0445f7b154143f11551e2f66864d94f7b26b45f.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1519,12 +1519,8 @@ static int __mdss_fb_display_thread(void *data)
 		pr_info("%s: set priority failed\n", __func__);
 
 	while (1) {
-		ret = wait_event_interruptible(mfd->commit_wait_q,
+		wait_event(mfd->commit_wait_q,
 				atomic_read(&mfd->commits_pending));
-		if (ret) {
-			pr_info("%s: interrupted", __func__);
-			continue;
-		}
 
 		ret = __mdss_fb_perform_commit(mfd);
 
